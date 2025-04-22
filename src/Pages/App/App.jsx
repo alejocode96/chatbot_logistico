a7270e4324a95ec4f4563892f26c67e6d63df1c7
alejocode96 <dgaleano28@argos.com.co>
2025-04-21 19:09:05 -0500

    Inicialización proyecto

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-500'>
+        <h1>HOLA BIENVENIDO A LOGISTICO</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
